ee2ee27869b97c16a4075d78e15ad0b7b548db45
Dod900ls1 <boyaregor1@gmail.com>
2024-02-22 16:57:10 +0000

    Added speed field in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    private int speed = 100;
+    private static int speed = 100;
     public static void main(String[] args) {
         Renderer renderer = Renderer.getInstance();
 
         while (true) {
             renderer.updateGrid();
             try {
-                Thread.sleep(100); // Add a delay to observe the changes
+                Thread.sleep(speed); // Add a delay to observe the changes
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
