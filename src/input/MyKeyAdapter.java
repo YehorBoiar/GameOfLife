060729bce168ee8256d6b82a6b369064ab790bd6
Dod900ls1 <boyaregor1@gmail.com>
2024-02-25 16:16:56 +0000

    Handled the case when we try to draw on a square number 0 (previously we had an exception).

---
+++ src/input/MyKeyAdapter.java
@@ -0,0 +1,44 @@
+package input;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import fileio.Load;
+import fileio.Save;
+import renderer.Renderer;
+
+/**
+ * MyKeyAdapter is responsible for adding hotkeys to our program.
+ */
+public class MyKeyAdapter extends KeyAdapter{
+    private final Renderer renderer;
+
+    public MyKeyAdapter(Renderer renderer) {
+        this.renderer = renderer;
+    }
+    
+    /**
+     * In this method we add hotkeys we want to have,
+     * and assign functionality to them.
+     */
+    @Override
+    public void keyPressed(KeyEvent e) { 
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            toggleGameState();
+        }
+        if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_S) {
+            Save save = new Save(renderer.getGrid());
+            save.SaveToFile();
+        }
+        if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_O) {
+            Load load = new Load();
+            load.loadFile();
+        }
+    }
+
+    private void toggleGameState() {
+        renderer.setGameState(!renderer.getGameState());
+        System.out.println("Game State toggled to: " + renderer.getGameState());
+    }
+
+    // TODO - Write CTRL-Q to exit the program.
+}
